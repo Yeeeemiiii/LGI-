68518dcce6f889679cf7c518420699e8eef52a72
Yemi <abdulrahmman1233@gmail.com>
2026-07-06 21:47:58 +0200

    chore: updated API keys from Firebase after GitHub mail

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // REPLACE THIS ENTIRE OBJECT WITH YOUR UNIQUE KEYS FROM PHASE 2
 const firebaseConfig = {
-  apiKey: "AIzaSyB7Uy8hnz6wDKJNLajr491ljBdqNBrVmTc",
-  authDomain: "lgi-foundation.firebaseapp.com",
-  projectId: "lgi-foundation",
-  storageBucket: "lgi-foundation.firebasestorage.app",
-  messagingSenderId: "686821960935",
-  appId: "1:686821960935:web:029aef6014539ae83a8642"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
